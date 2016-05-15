17c7c84cc409f0a892b699afb5e7f2d716b623fa
brent.fisher@careerbuilder.com <brent.fisher@careerbuilder.com>
2016-05-15 18:26:47 -0400

    add have a party function, with modifiers to homebrew items

--- index.js
+++ index.js
@@ -23,11 +23,14 @@ var logBox = grid.set(0, 3, 4, 9, blessed.log, {
 
 var dollarsBox = null;
 var beersDrankBox = null;
+var beerStock = null;
 var itemBox = null;
 var listBox = null;
 var priceBox = null;
 var descBox = null;
 var buyPrompt = null;
+
+var modifiers = {};
 
 var activityBox = grid.set(4, 0, 4, 3, blessed.text, {
   label: "brewery",
@@ -44,11 +47,10 @@ activityBox.on('click', function(mouse) {
   // activityBox.setContent('You clicked ' + mouse.x + ', ' + mouse.y + '.');
   logBox.log("You drink a beer, you feel happy");
   clicks = clicks + 1;
-  if (dollarsBox)
-    dollarsBox.content = "$ " + dollars.toString();
+  if (beersDrankBox)
+    beersDrankBox.content = clicks.toString();
   // if (listBox)
   //   listBox.focus();
-  logic(clicks);
   screen.render();
 });
 
@@ -58,11 +60,12 @@ var counter = 0;
 clicks = 0;
 dollars = 0;
 // setInterval(GameLoop, 1000 / fps);
-setInterval(GameLoop, 1000);
+setInterval(GameLoop, 500);
 
 function GameLoop() {
   counterBox.content = counter.toString();
   // dollarsBox.content = counter.toString();
+  logic(clicks, counter);
   screen.render();
   counter++;
 }
@@ -87,16 +90,24 @@ function beerASCII() {
 
 var drawDollars = function() {
   dollarsBox = grid.set(8, 0, 1, 2, blessed.text, {
-    label: "dollars",
-    content: "0"
+    label: "Dollars",
+    content: "50"
   });
   screen.render();
 }
 
 var drawBeersDrank = function() {
   beersDrankBox = grid.set(9, 0, 1, 2, blessed.text, {
-    label: "beers drank",
+    label: "Beers Drank",
     content: "0"
+  });
+  screen.render();
+}
+
+var drawBeerStock = function() {
+  beerStock = grid.set(10, 0, 1, 2, blessed.text, {
+    label: "Beer Stock",
+    content: "1"
   });
   screen.render();
 }
@@ -151,6 +162,7 @@ var drawShop = function() {
       if (value) {
         logBox.log("Bought item: " + item);
         listBox.removeItem(item);
+        itemAction(item);
         descBox.content = "";
         priceBox.content = "";
         if (!itemBox) {
@@ -180,6 +192,9 @@ function getPrice(item) {
     case "placeholder":
       output = 0;
       break;
+    case "Have a party":
+      output = 20;
+      break;
   }
   return output;
 }
@@ -192,6 +207,9 @@ function getDesc(item) {
       break;
     case "placeholder":
       output = "nothing to see here";
+      break;
+    case "Have a party":
+      output = "Have a party and drink some beer! Maybe your friends have some brewing tips.";
       break;
   }
   return output;
@@ -213,14 +231,93 @@ function initPrompts() {
   screen.render();
 }
 
-function logic(clicks) {
+function logic(clicks, counter) {
   if (clicks) {
-    if (clicks == 1) {
+    if (clicks == 1 && logBox.content.indexOf('You feel drunk,') < 1) {
       logBox.log("You feel drunk, maybe we should start brewing this stuff...");
       drawBeersDrank();
       drawDollars();
       drawShop();
     }
-  } else { 
+  }
+  itemsLogic();
+  if (beerStock) {
+    var beerStockCount = parseInt(beerStock.content.toString());
+    if (beerStockCount > 1) {
+      if (!doesListBoxHave("Have a party") && !doesHaveItem("Have a party")) {
+        logBox.log("Hey, we have all this beer... Let's have a party!");
+        listBox.insertItem(0, "Have a party");
+      }
+    }
+  } 
+}
+
+function itemsLogic() {
+  var items = getItems();
+  if (items) {
+    if (items.indexOf("HomeBrew Kit") > -1) {
+      logBox.log("you brew some beer");
+      if (!beerStock) {
+        drawBeerStock();
+      } else {
+        var beers = getHomeBrewRate(counter);
+        changeBeers(beers);
+      }
+    }
+  }
+}
+
+function getItems() {
+  if (itemBox) {
+    return itemBox.content.split("\n");
+  } else {
+    return null;
+  }
+}
+
+function doesHaveItem(str) {
+  var myItems = getItems();
+  if (myItems.indexOf(str) > -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function doesListBoxHave(str) {
+  var shopItems = listBox.getItemIndex(str)
+  if (shopItems > -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getHomeBrewRate(counter) {
+  var modifier = modifiers.HomeBrewRate || 1;
+  if (counter % 5 == 0)
+    return 1 * modifier;
+  else
+    return 0;
+}
+
+function changeDollars(amount) {
+  var dollars = parseInt(dollarsBox.content.toString());
+  dollars = dollars + amount;
+  dollarsBox.content = dollars.toString();
+  screen.render();
+}
+
+function changeBeers(beers) {
+  var strBeers = beerStock.content = beerStock.content.toString();
+  beerStock.content = (parseInt(strBeers) + beers).toString();
+  screen.render();
+}
+
+function itemAction(item) {
+  if (item == "Have a party") {
+    logBox.log("Smeffdegg gives you some tips, you can now brew beer twice as fast!");
+    modifiers.HomeBrewRate = 2;
+    changeDollars(-20);
   }
 }
